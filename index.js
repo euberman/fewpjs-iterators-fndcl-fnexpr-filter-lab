4c4fb49416fbfdac28a8d564c10e2aeb6a45cf59
euberman <euberman@gmail.com>
2020-11-23 12:21:28 -0600

    commit tree 9f5ee700f264ae09dad5f2f6bd13c8c6c0b8f941

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatch(drivers, string){
+  drivers.filter(driver => driver === string);
+}
+
+function fuzzyMatch(drivers, string){
+  drivers.filter(driver => driver.startsWith(string));
+}
+
+function matchName(drivers, string){
+  drivers.filter( driver => driver.name === string);
+}
